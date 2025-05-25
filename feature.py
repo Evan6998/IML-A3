8da8042ae0063cdd55c81b19524efd6b1f7c9007
Evan6998 <hyf6998@icloud.com>
2025-05-25 12:55:30 -0400

    feature.py

--- feature.py
+++ feature.py
@@ -1,6 +1,7 @@
 import csv
 import numpy as np
 import argparse
+import typing
 
 VECTOR_LEN = 300   # Length of glove vector
 MAX_WORD_LEN = 64  # Max word length in dict.txt and glove_embeddings.txt
@@ -28,6 +29,27 @@ def load_tsv_dataset(file):
     return dataset
 
 
+def review_to_vector(review: str, dictionary: dict[str, typing.Any]):
+    x = review.split()
+    x_trim = [word for word in x if word in dictionary]
+    N = len(x_trim)
+    return np.sum([dictionary[word] for word in x_trim], axis=0) / N
+
+
+def embedding(data: str, glove_file):
+    dataset = load_tsv_dataset(data)
+    dictionary = load_feature_dictionary(glove_file)
+
+    labels = np.array([row[0] for row in dataset], dtype=int).reshape(-1, 1)
+    
+    reviews = np.array([row[1] for row in dataset], dtype=str)
+    feature_matrix = np.array([review_to_vector(review, dictionary) for review in reviews])
+
+    print(f"{labels.shape=}")
+    print(f"{feature_matrix.shape=}")
+    return np.concatenate([labels.reshape(-1, 1), feature_matrix], axis=1)  # shape (8, 301)
+
+
 def load_feature_dictionary(file):
     """
     Creates a map of words to vectors using the file that has the glove
@@ -49,6 +71,13 @@ def load_feature_dictionary(file):
     return glove_map
 
 
+def output_matrix(data, file):
+    with open(file, 'w') as fout:
+        for row in data:
+            fout.write("\t".join([f"{round(float(v), 6):.6f}" for v in row]))
+            fout.write("\n")
+
+
 if __name__ == '__main__':
     # This takes care of command line argument parsing for you!
     # To access a specific argument, simply access args.<argument name>.
@@ -66,3 +95,13 @@ if __name__ == '__main__':
     parser.add_argument("test_out", type=str, 
                         help='path to output .tsv file to which the feature extractions on the test data should be written')
     args = parser.parse_args()
+
+
+    d = embedding(args.train_input, args.feature_dictionary_in)
+    output_matrix(d, args.train_out)
+
+    d = embedding(args.validation_input, args.feature_dictionary_in)
+    output_matrix(d, args.validation_out)
+
+    d = embedding(args.test_input, args.feature_dictionary_in)
+    output_matrix(d, args.test_out)
